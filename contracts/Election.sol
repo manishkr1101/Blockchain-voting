e212162cfbd20262fa88460cf87702fc7d96abec
Manish Kumar <manishkumarsh120@gmail.com>
2021-10-29 20:04:12 +0530

    Modified contract for election

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,20 +2,40 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Election {
-    mapping(uint => Candidate) public candidates;
+    
     struct Candidate {
         uint id;
         string name;
-        uint votes;
+        uint voteCount;
     }
 
+    uint public candidatesCount;
+    mapping(uint => Candidate) public candidates;
+    mapping(address => bool) private voted;
+
+    event electionUpdated (
+        uint id,
+        string name,
+        uint voteCount
+    );
+
     constructor() public {
-        addCandidate(0, "Obama", 0);
-        addCandidate(1, "Biden", 0);
+        addCandidate("Obama");
+        addCandidate("Biden");
     } 
 
-    function addCandidate(uint _id, string memory _name, uint _votes) private {
-        Candidate memory cand = Candidate(_id, _name, _votes);
-        candidates[_id] = cand;
+    function addCandidate(string memory _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote(uint _id) public {
+        require(voted[msg.sender] == false, "Already voted");
+        require(candidates[_id].id != 0, "the id does not exist");
+
+        candidates[_id].voteCount += 1;
+        voted[msg.sender] = true;
+        
+        emit electionUpdated(_id, candidates[_id].name, candidates[_id].voteCount);
     }
 }
